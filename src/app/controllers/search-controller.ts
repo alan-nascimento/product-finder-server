fc535855e20e4aeb5b44a3ed65d2bc4e6c91b2be
Alan Nascimento <alan.silva.n@hotmail.com>
2020-10-07 15:50:31 -0300

    feat: return expected values  from search controller

--- src/app/controllers/search-controller.ts
+++ src/app/controllers/search-controller.ts
@@ -5,13 +5,41 @@ import http from '@/config/http-client';
 class SearchController {
   public async search(req: Request, res: Response): Promise<Response> {
     try {
-      const response = await http.get('/sites/MLA/search', {
+      const { data } = await http.get('/sites/MLU/search', {
         params: { q: req.query.search },
       });
 
-      return res.status(200).json(response.data);
-    } catch (err) {
-      return res.status(500).json(err);
+      if (!data.results.length) {
+        return res.status(204).json(data.results);
+      }
+
+      const result = {
+        author: {
+          name: 'Alan',
+          lastname: 'Nascimento',
+        },
+        categories: data.filters[0].values[0].path_from_root.map(
+          (item: any) => item.name
+        ),
+        items: data.results.slice(0, 4).map((item: any) => ({
+          id: item.id,
+          title: item.title,
+          price: {
+            id: item.id,
+            currency: item.currency_id,
+            amount: item.installments.amount,
+            decimals: 'TO_IMPLEMENT',
+          },
+          picture: item.thumbnail,
+          condition: item.condition,
+          free_shipping: item.shipping.free_shipping,
+          city: item.address.city_name,
+        })),
+      };
+
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).json(error);
     }
   }
 }
