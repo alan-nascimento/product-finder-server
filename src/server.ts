576d267f9c9bdc252aba1bebc92cde18d9119520
Alan Nascimento <alan.silva.n@hotmail.com>
2020-10-11 01:01:06 -0300

    fix: set a default port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port: ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
